ca5db5f4cc4231f9323760fc1c35cfbe661a37df
Alice Li <nameal1234@gmail.com>
2024-08-30 13:00:10 -0500

    claid.org

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://claid-frontend.vercel.app", // Allow requests from this specific origin
+    origin: "https://www.claid.org/", // Allow requests from this specific origin
   })
 );
 app.use(express.json());
